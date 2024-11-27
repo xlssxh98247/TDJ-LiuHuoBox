bff8bbad1e6ecb4b5ee9c9b72e6107e3419bdc30
GrandFuga <xlssxh98247@163.com>
2024-11-28 02:14:42 +0800

    push new code

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import firstpage from './components/firstpage.vue';
+</script>
+
+<template>
+  <div>
+    <firstpage />
+  </div>
+</template>
+
+<style scoped>
+  body {
+    margin:0;
+	  padding:0;
+	  border:0
+  }
+</style>
